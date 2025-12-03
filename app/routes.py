d2e90ce5c2bcc051070d912df418142fac1fde53
peeepy <bubbles.splat@gmail.com>
2025-12-03 11:57:03 +0000

    step03: added index template & rendering in route

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
+from flask import render_template
 
 bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
 def index():
-    return "<h1>Todo index page"
+    return render_template("index.html")
 
 @bp.route("/")
 def all_tasks():

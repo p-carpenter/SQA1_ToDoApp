18e21e652d8acc3845b7f6b979081726b40aeb67
peeepy <bubbles.splat@gmail.com>
2025-12-03 13:35:47 +0000

    Step 13: Added delete task functionality

--- app/index/routes.py
+++ app/index/routes.py
@@ -15,7 +15,10 @@ def all_tasks():
 
 @bp.route("/task/<int:task_id>")
 def task(task_id):
-    task = todos[task_id - 1]
+    task = None
+    for todo in todos:
+        if todo["id"] == task_id:
+            task = todo
     return render_template("task.html", task=task)
 
 @bp.route("/edit-task/<int:task_id>", methods=["GET", "POST"])
@@ -35,6 +38,30 @@ def edit_task(task_id):
 
     return render_template("task_form.html", task=task)
 
-@bp.route("/create-task")
+
+@bp.route("/create-task", methods=["GET", "POST"])
 def create_task():
-    return render_template("create_task.html")
+    if request.method == "POST":
+        task_id = todos[-1]["id"] + 1
+        title = request.form.get("title")
+        description = request.form.get("description")
+        tags_list = request.form.get("tags", "")
+        tags = [t.strip() for t in tags_list.split(",") if t.strip()]
+        todos.append({
+            "id": task_id,
+            "title": title,
+            "description": description,
+            "created_at": datetime.now(),
+            "tags": tags
+        })
+        return redirect(url_for("main.task", task_id=task_id))
+    return render_template("task_form.html", task=None)
+
+
+@bp.route("/delete-task/<int:task_id>", methods=["Post"])
+def delete_task(task_id):
+    for i, todo in enumerate(todos):
+        if todo["id"] == task_id:
+            todos.pop(i)
+            break
+    return redirect(url_for("main.all_tasks"))
